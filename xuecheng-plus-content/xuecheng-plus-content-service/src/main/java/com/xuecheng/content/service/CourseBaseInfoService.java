30a388a6d6b6944d725446b035adc9a5f8b1b3fd
SS <944176085@qq.com>
2023-11-03 21:33:25 +0800

    DAO接口

---
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/CourseBaseInfoService.java
@@ -0,0 +1,10 @@
+package com.xuecheng.content.service;
+
+/**
+ * @Author: SS
+ * @DATE: 2023/11/3 21:32
+ * @Decription: TODO
+ * @Version 1.0
+ **/
+public interface CourseBaseInfoService {
+}
---
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/test/java/com/xuecheng/ContentApplication.java
@@ -0,0 +1,17 @@
+package com.xuecheng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: SS
+ * @DATE: 2023/11/3 17:16
+ * @Decription: 内容管理服务启动类
+ * @Version 1.0
+ **/
+@SpringBootApplication
+public class ContentApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ContentApplication.class,args);
+    }
+}
