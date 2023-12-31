7b724f060dff23b0140a1fdb33ec2c14262dd14d
SS <944176085@qq.com>
2023-12-31 16:50:16 +0800

    JRS303分组异常处理

--- xuecheng-plus-base/src/main/java/com/xuecheng/base/exception/GlobalExceptionHandler.java
+++ xuecheng-plus-base/src/main/java/com/xuecheng/base/exception/GlobalExceptionHandler.java
@@ -1,8 +1,16 @@
 package com.xuecheng.base.exception;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @Author: SS
@@ -40,5 +48,26 @@ public class GlobalExceptionHandler {
         // 解析出异常信息
         String errMessage = e.getMessage();
         return new RestErrorResponse(CommonError.UNKOWN_ERROR.getErrMessage());
+    }
+
+    // MethodArgumentNotValidException
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public RestErrorResponse methodArgumentNotValidException(MethodArgumentNotValidException e) {
+
+        BindingResult bindingResult = e.getBindingResult();
+        // 存放错误信息
+        List<String> errors = new ArrayList<>();
+        bindingResult.getFieldErrors().stream().forEach(item->{
+            errors.add(item.getDefaultMessage());
+        });
+
+        // 将list中的错误信息拼接
+        String errMessage = StringUtils.join(errors, ",");
+
+        // 记录异常
+        log.error("系统异常{}", e.getMessage(), errMessage);
+
+        return new RestErrorResponse(errMessage);
     }
 }
---
+++ xuecheng-plus-base/src/main/java/com/xuecheng/base/exception/ValidationGroups.java
@@ -0,0 +1,13 @@
+package com.xuecheng.base.exception;
+
+/**
+ * @Author: SS
+ * @DATE: 2023/12/31 16:45
+ * @Decription: 用于分组校验
+ * @Version 1.0
+ **/
+public class ValidationGroups {
+    public interface Inster{};
+    public interface Update{};
+    public interface Delete{};
+}
